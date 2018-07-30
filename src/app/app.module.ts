b5511cf7821bbff3cca2a105f906ff6f59666af7
Arnab S Das <arnabd413@IN.nam.ad.pwcinternal.com>
2018-07-30 20:24:00 +0530

    Design Done with html & css in single component in root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { RequestComponent } from './request/request.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    RequestComponent
   ],
   imports: [
     BrowserModule
