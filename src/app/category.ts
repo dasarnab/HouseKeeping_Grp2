478945d01995b6eb59f6773a40a0e77844e93d10
Arnab S Das <arnabd413@IN.nam.ad.pwcinternal.com>
2018-08-05 12:15:51 +0530

    category Service done

---
+++ src/app/category.ts
@@ -0,0 +1,15 @@
+export interface ICategories {
+    serviceTypeId: number;
+    serviceTypeDesc: string;
+    price: number;
+    parentServiceTypeId: string;
+}
+
+// export interface ICategories {
+//     userId: number;
+//     firstName: string;
+//     lastName: string;
+//     email: string;
+//     mobileNumber: string;
+
+// }
